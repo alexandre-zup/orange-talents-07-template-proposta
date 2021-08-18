14940f26ba658e869c283b46482f3971f010aaab
alexandre-zup <alexandre.santos@zup.com.br>
2021-08-18 00:14:20 -0300

    refatorando avalicao financeira

--- src/main/java/br/com/zupacademy/propostas/controllers/PropostaController.java
+++ src/main/java/br/com/zupacademy/propostas/controllers/PropostaController.java
@@ -1,19 +1,20 @@
 package br.com.zupacademy.propostas.controllers;
 
-import br.com.zupacademy.propostas.apiclients.avaliacaofinanceira.AvaliacaoRequest;
-import br.com.zupacademy.propostas.apiclients.avaliacaofinanceira.AvaliacaoResponse;
-import br.com.zupacademy.propostas.apiclients.avaliacaofinanceira.AvaliacaoService;
+import br.com.zupacademy.propostas.apiclients.avaliacaofinanceira.*;
 import br.com.zupacademy.propostas.controllers.dto.request.NovaPropostaRequest;
 import br.com.zupacademy.propostas.controllers.exception.ValidationErrorsOutputDto;
 import br.com.zupacademy.propostas.controllers.exception.exceptions.UnprocessableEntityException;
 import br.com.zupacademy.propostas.model.entities.Proposta;
 import br.com.zupacademy.propostas.model.repositories.PropostaRepository;
+import feign.FeignException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.transaction.Transactional;
@@ -27,7 +28,7 @@ public class PropostaController {
     @Autowired
     private PropostaRepository repository;
     @Autowired
-    private AvaliacaoService avaliacaoService;
+    private AvaliacaoClient client;
 
     @PostMapping
     @Transactional
@@ -42,13 +43,27 @@ public class PropostaController {
         }
 
         repository.save(proposta);
-
-        AvaliacaoRequest avaliacaoRequest = new AvaliacaoRequest(proposta.getDocumento(),
-                proposta.getNome(), proposta.getId().toString());
-        AvaliacaoResponse response = avaliacaoService.avalia(avaliacaoRequest);
-        proposta.atualizaEstado(response.getResultadoSolicitacao());
+        ResultadoSolicitacao resultado = avalia(proposta);
+        proposta.atualizaEstado(resultado);
 
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(proposta.getId()).toUri();
         return ResponseEntity.created(uri).build();
+    }
+
+    private ResultadoSolicitacao avalia(Proposta proposta) throws ResponseStatusException {
+        AvaliacaoRequest request = new AvaliacaoRequest(proposta.getDocumento(),
+                proposta.getNome(), proposta.getId().toString());
+
+        AvaliacaoResponse response;
+
+        try {
+            return client.avalia(request).getResultadoSolicitacao();
+        } catch (FeignException ex) {
+            if (ex.status() == HttpStatus.UNPROCESSABLE_ENTITY.value()) {
+                return ResultadoSolicitacao.COM_RESTRICAO;
+            } else {
+                throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Erro durante a avaliação da proposta");
+            }
+        }
     }
 }
